7970848fb6c19f12c698270501440b3d9f711e4c
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2024-03-20 12:12:19 +0100

    Update hooks.py

--- frappe_paystack/hooks.py
+++ frappe_paystack/hooks.py
@@ -173,5 +173,5 @@ user_data_fields = [
 # 	"frappe_paystack.auth.validate"
 # ]
 
-required_apps = ["frappe", "erpnext", "payments", "ecommerce_integrations"]
+required_apps = ["frappe", "erpnext", "ecommerce_integrations"]
 
